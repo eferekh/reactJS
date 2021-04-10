5ed4245bc0c437217535fb611adeb709a8687df7
Ely Fe <eferekh@eFerekh.local>
2021-04-10 15:53:22 +0300

    Added routing to vidly project

--- mosh-app/Vidly-Project-V2/src/App.js
+++ mosh-app/Vidly-Project-V2/src/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
 import Movies from "./Components/Movies";
+import Customers from "./Components/Customers";
+import Rentals from "./Components/Rentals";
+import NotFound from "./Components/NotFound";
+import NavBar from "./Components/Navbar";
+import MovieForm from './Components/MovieForm';
 
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
@@ -8,9 +14,20 @@ class App extends Component {
     state = {};
     render() {
         return (
-            <div className="container">
-                <Movies />
-            </div>
+            <>
+                <NavBar />
+                <main className="container">
+                    <Switch>
+                        <Route path="/movies/:id" component={MovieForm} />
+                        <Route path="/movies" component={Movies} />
+                        <Route path="/customers" component={Customers} />
+                        <Route path="/rentals" component={Rentals} />
+                        <Route path="/not-found" component={NotFound} />
+                        <Redirect from="/" exact to="/movies" />
+                        <Redirect to="/not-found" />
+                    </Switch>
+                </main>
+            </>
         );
     }
 }
